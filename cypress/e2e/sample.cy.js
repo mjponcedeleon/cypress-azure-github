7747b78b2f71ee0114f3155c43d605f8120b3456
MJEPDL <mjponcedeleon@outlook.com>
2025-06-20 19:29:13 +0800

    Added screenshot

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -2,5 +2,6 @@ describe('Homepage Test', () => {
   it('Should load the homepage and verify title', () => {
     cy.visit('https://example.cypress.io')
     cy.contains('Kitchen Sink')
+    cy.screenshot('homepage');
   })
 })
